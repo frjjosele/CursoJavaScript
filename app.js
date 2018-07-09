955a2d80688c309915b2bff6e577385dce2d628e
josele <josele@Josele-PC>
2018-07-09 20:49:11 +0200

    primer contenido

---
+++ app.js
@@ -0,0 +1,36 @@
+// const numero1=30;
+// const numero2=20;
+// const numero3=2.20;
+// const numero4=.1020;
+// const numero5=-3;
+
+//Otra forma de declararlos
+const numero1=30,
+ numero2=20,
+ numero3=2.20,
+ numero4=.1020,
+ numero5=-3;
+
+let resultado;
+
+resultado=numero1+numero2;
+
+
+resultado=Math.PI;
+console.log(resultado);
+resultado=Math.round(2.4); //2 -->Redondea al entero mas cercano 
+console.log(resultado);
+resultado=Math.floor(2.8); //2 -->Redondea al entero inferior mas cercano
+console.log(resultado);
+resultado=Math.ceil(2.1); //3 -->Redondea al enterio superior mas cercano
+console.log(resultado);
+resultado=Math.abs(-3); //3 valor absoluto
+console.log(resultado);
+resultado=Math.min(3,6,8,1,-6,0); //-6-->Devuelve el valor mas pequeño del array
+console.log(resultado);
+resultado=Math.max(3,6,8,1,-6,0); //8 -->Devulve el valor mas grande e un array
+console.log(resultado);
+resultado=Math.random();//0.03715101385965769-->numero aleatorio entre 0 y 1
+console.log(resultado);
+
+
