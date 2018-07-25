1906c20244f85df956166fa6a2ed8a86dd9ded01
josele <josele@Josele-PC>
2018-07-26 00:08:18 +0200

    seccion 4 terminada

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,19 +1,20 @@
-//click en el buscador
-document.querySelector('#submit-buscador').addEventListener('click',function(e){
-    e.preventDefault();//detenemos la accion por defecto del elemento. Por ejemplo el submit por defecto va a llevarte a la pagina que se encuentra en el action del form. Como en esta ocasion no queremos eso por eso lo paramos
-    alert("Buscando cursos");
-});
+document.body.addEventListener('click',eliminarElemento);
 
-function ejecutarBoton(e){
+function eliminarElemento(e){
     e.preventDefault();
-    console.log("Desde la funcion ejecutar boton");
 
-    console.log(e);//Nos muestra la informacion relacionada con el evento
+    console.log('click');
 
-    e.target;//target nos muestra el elemento al que hemos realizado en evento
-    console.log(e.target);
+    console.log(e.target.classList);
 
-    console.log(e.target.ClassName);
 
-    console.log(e.target.innerText);
+//Como hemos seleccionado el body, solo queremos que actuen sobre los elementos de la clase borrar-curso. Para eso usamos el constains('nombre de la clase') del classList
+    if(e.target.classList.contains('borrar-curso')){
+        console.log(e.target.parentElement.parentElement.remove());
+    }
+
+    if(e.target.classList.contains('agregar-carrito')){
+        console.log('curso agregado');
+    }
+
 }
